21d89d84271e4ca172ebcf8d76756000308277f4
Bhumi Patel <bhumidpatel224@gmail.com>
2024-08-10 21:30:13 +0530

    third commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.7s ease-in-out",
+        fadeOut: "fadeOut 0.7s ease-in-out",
+      },
+      colors: {
+        custom: {
+          DEFAULT: "#000000",
+          light: "#ffffff",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("tw-elements/dist/plugin"),
+    require("tailwind-scrollbar"),
+  ],
+};
